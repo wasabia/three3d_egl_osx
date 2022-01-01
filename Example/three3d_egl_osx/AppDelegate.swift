2145c66fb18a78b6789ddebafff639766256ddd0
zl <themorecolor@gmail.com>
2022-01-01 22:04:53 +0800

    Initial commit

---
+++ Example/three3d_egl_osx/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  three3d_egl_osx
+//
+//  Created by zl on 01/01/2022.
+//  Copyright (c) 2022 zl. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Example/three3d_egl_osx/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  three3d_egl_osx
+//
+//  Created by zl on 01/01/2022.
+//  Copyright (c) 2022 zl. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
